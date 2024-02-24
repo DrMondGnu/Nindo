df9bd817222f65eb9bb1da8cd8c6b2776f715561
MondGnu <mondsuppe24@gmail.com>
2024-02-24 11:35:23 +0100

    fixed mondengine cmake including and other stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+#include "engine/log.h"
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    mondengine::Log::init();
+    APP_INFO("Hello from spdlog");
     return 0;
 }
